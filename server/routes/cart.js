945ae66cbae6a9e23eec06d3ce930535426c4a13
Kevin J.H Li <tensaikev@gmail.com>
2022-11-15 18:23:59 -0500

    updated files

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -5,7 +5,8 @@ const client = require("../elephant");
 
 cartRouter.post("/", async (request, response) => {
   const { title, quantity, price } = request.body;
-  console.log(title, quantity, price);
+  //   console.log(title, quantity, price);
+  console.log(request.cookies);
 
   //   response.status(201).send(request.sessionID);
   response.status(201).send(request.cookies);
