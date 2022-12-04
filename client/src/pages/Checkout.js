785b8ff5b6cecd45e2078df412c2127a21d043b4
Kevin J.H Li <tensaikev@gmail.com>
2022-12-04 01:22:55 -0500

    updated files

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react"
 import CartItem from "../components/CartItem"
 import { getAllCartItems } from "../services/cart"
+import { useTotalPriceStore } from "../stateStore"
 
 const Checkout = () => {
   const [cartItems, setCartItems] = useState([])
+
+  // The global states we stored
+  const cartPrice = useTotalPriceStore((state) => state.totalPrice)
 
   useEffect(() => {
     const getCartItems = async () => {
@@ -19,7 +23,7 @@ const Checkout = () => {
 
   return (
     <>
-      <div className="flex mt-12 w-1/2 m-auto gap-4 h-2/5">
+      <div className="flex mt-12 w-1/2 m-auto gap-4 h-3/5">
         <div className="w-full">
           <h2>Checkout form </h2>
           <h2>Checkout form </h2>
@@ -27,13 +31,22 @@ const Checkout = () => {
           <h2>Checkout form </h2>
           <h2>Checkout form </h2>
         </div>
-        <ul className="w-full overflow-y-auto h-full">
-          {cartItems.length === 0 && <h3>NO ITEMS IN CART</h3>}
-          {cartItems.length > 0 &&
-            cartItems.map((element) => (
-              <CartItem key={element.id} {...element} />
-            ))}
-        </ul>
+        <div className="divider divider-horizontal"></div>
+
+        {/* Add overflow-y-auto to add scrolling once the items overflow the given height. It will have
+        a scroll bar  */}
+        <div className="w-full">
+          <ul className="overflow-y-auto h-full">
+            {cartItems.length === 0 && <h3>NO ITEMS IN CART</h3>}
+            {cartItems.length > 0 &&
+              cartItems.map((element) => (
+                <CartItem key={element.id} {...element} />
+              ))}
+          </ul>
+          <div className="divider"></div>
+
+          <p>$ Total Cost: {cartPrice}</p>
+        </div>
       </div>
     </>
   )
