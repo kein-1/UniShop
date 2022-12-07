31044d6733e898f5c33f55bfe23c34a454324114
Kein L <tensaikev@gmail.com>
2022-12-07 22:47:01 +0000

    Update users.js

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -1,11 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
 // const baseUrl = "http://localhost:3001/api/users"
-const baseUrl = "https://51mw8f-3001.preview.csb.app/api/users"
+const baseUrl = "https://51mw8f-3001.preview.csb.app/api/users";
+
+const loginUser = async (userInfo) => {
+  const { data } = await axios.post(`baseurl/${login}`);
+  return data;
+};
 
 const registerUser = async (userInfo) => {
-  const { data } = await axios.post(baseUrl, userInfo)
-  return data
-}
+  const { data } = await axios.post(baseUrl, userInfo);
+  return data;
+};
 
-export default registerUser
+export default registerUser;
